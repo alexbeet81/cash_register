155f6b39ce18423abf2c89310ca27998f061c1fc
Alex Pritchard <alexbeet1234@gmail.com>
2023-09-20 18:15:15 +0100

    removed new_product code from ProductRepositroy

--- spec/services/discount_service_spec.rb
+++ spec/services/discount_service_spec.rb
@@ -56,4 +56,40 @@ describe DiscountService do
       expect(checkout.total).to eq(10.00)
     end
   end
+
+  describe "#apply_discount percentage" do
+    let(:product) { Product.new("CF1", "Coffee", 11.23) }
+    let(:discount) { Discount.new(product_code: "SR1", product_threshold: 3, discount_type: "fixed", product_price: 5.00, fixed_discount: 0.5)}
+    let(:checkout) { Checkout.new }
+
+    let(:service) { described_class.new(product, discount, checkout) }
+
+    it "applys the correct discount after the threshold has been reached" do
+      expect(checkout.basket.products.length).to eq(0)
+      expect(product.price).to eq(5.00)
+
+      3.times do
+        checkout.scan(product)
+      end
+
+      service.apply_discount
+      expect(product.price).to eq(4.50)
+      expect(checkout.basket.products.length).to eq(3)
+      expect(checkout.total).to eq(13.50)
+    end
+
+    it "doesn't apply the discount before the threshold has been reached" do
+      expect(checkout.basket.products.length).to eq(0)
+      expect(product.price).to eq(5.00)
+
+      2.times do
+        checkout.scan(product)
+      end
+
+      service.apply_discount
+      expect(product.price).to eq(5.00)
+      expect(checkout.basket.products.length).to eq(2)
+      expect(checkout.total).to eq(10.00)
+    end
+  end
 end
